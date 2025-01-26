c04ea0e5251cbdbf7f2894899556b9eb6a5e672e
elianollivier <elian.ollivier@etu.unilim.fr>
2025-01-26 11:12:26 +0100

    Milieu TP1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.content.Intent
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +17,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LayoutPrincipal)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val layoutPrincipal: ConstraintLayout = findViewById(R.id.LayoutPrincipal)
+
+        val editText: EditText = findViewById(R.id.editTextText)
+
+        val buttonStart: Button = findViewById(R.id.button_start)
+
+        buttonStart.setOnClickListener {
+            val userInput = editText.text.toString().trim()
+
+            if (userInput == "afficher nouveau textView") {
+                val deuxiemeTextView = TextView(this).apply {
+                    text = userInput
+                    textSize = 18f
+                }
+
+                layoutPrincipal.addView(deuxiemeTextView)
+
+                Toast.makeText(this, "Nouveau TextView créé !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Bouton cliqué !", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val buttonNext = findViewById<Button>(R.id.button_next)
+
+        buttonNext.setOnClickListener {
+            val userText = editText.text.toString().trim()
+
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra(EXTRA_TEXT, userText)
+
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val EXTRA_TEXT = "text_to_display"
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myapplication.databinding.ActivityMain2Binding
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val intent = intent
+
+        val textToDisplay = intent.getStringExtra(MainActivity.EXTRA_TEXT) ?: ""
+
+        val textViewDisplay = findViewById<TextView>(R.id.textViewDisplay)
+        textViewDisplay.text = textToDisplay
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
